6b1eb63234d45cdc2998d6011148f6d421917220
Samir Mishra <samirmishra.a1@gmail.com>
2024-10-15 02:53:01 +0530

    style: remove trailing whitespace and fix indentation in boilerplate.rs
    Remove trailing whitespaces and fix indentation inconsistent in
    boilerplate.rs code.

--- src/boilerplates.rs
+++ src/boilerplates.rs
@@ -1,65 +1,58 @@
-pub const PYTHON_MAINFILE_CONTENT: &str = 
+pub const PYTHON_MAINFILE_CONTENT: &str =
 "import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!', 
-                   intents=discord.Intents.default())
-
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has logged in!')
-    
+
 
 @bot.command()
 async def ping(ctx: commands.Context):
     await ctx.send('Pong!')
-    
+
 
 bot.run('BOT_TOKEN')";
 
-pub const PYTHON_MAINFILE_COG_CONTENT: &str = 
+pub const PYTHON_MAINFILE_COG_CONTENT: &str =
 "import discord
 from discord.ext import commands
 
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix='!',
-                         intents=discord.Intents.default())
-        
-    
+        super().__init__(command_prefix='!', intents=discord.Intents.default())
+
     async def on_ready(self):
         print(f'{self.user.name} has logged in!')
-        
+
 
 bot = Bot()
 
 bot.run('TOKEN')";
 
-pub const PYTHON_COG_EXAMPLE_CONTENT: &str = 
+pub const PYTHON_COG_EXAMPLE_CONTENT: &str =
 "from discord.ext import commands
 
 class Example(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.Cog.listener()
     async def on_ready(self):
         print('Example cog has loaded!')
-        
-    
+
     @commands.command()
     async def ping(self, ctx: commands.Context):
         await ctx.send('Pong!')
-        
-        
+
 def setup(bot):
     bot.add_cog(Example(bot))";
 
 
-pub const JS_MAINFILE_CONTENT: &str = 
+pub const JS_MAINFILE_CONTENT: &str =
 "// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
@@ -76,7 +69,7 @@ client.once(Events.ClientReady, c => {
 // Log in to Discord with your client's token
 client.login(token);";
 
-pub const JS_MAINFILE_COG_CONTENT: &str = 
+pub const JS_MAINFILE_COG_CONTENT: &str =
 "// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
@@ -112,7 +105,7 @@ edition = '2018'
 serenity = { path = '../../', default-features = false, features = ['client', 'gateway', 'rustls_backend', 'model'] }
 tokio = { version = '1.0', features = ['macros', 'rt-multi-thread'] }";
 
-pub const RUST_MAINFILE_CONTENT: &str = 
+pub const RUST_MAINFILE_CONTENT: &str =
 "use std::env;
 
 use serenity::async_trait;
@@ -287,7 +280,7 @@ async fn main() {
     }
 }";
 
-pub const RUST_EXAMPLE_COG_CONTENT: &str = 
+pub const RUST_EXAMPLE_COG_CONTENT: &str =
 "use serenity::framework::standard::macros::command;
 use serenity::framework::standard::{Args, CommandResult};
 use serenity::model::prelude::*;
